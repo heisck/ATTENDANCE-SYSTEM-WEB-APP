6ee9e255160430f11ef9490e27e5169dd7679649
heisck <kelvinkwabenaparkingston@gmail.com>
2026-03-17 11:04:28 +0000

    ok

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.attendance.app',
   appName: 'Attendance App',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'http://10.55.5.178:3000',
+    cleartext: true
+  }
 };
 
 export default config;
